adcc80574f4da033e9834f9468e2b4ef66039d3f
Arasaka <otxoa.lardeux.pro@gmail.com>
2025-09-04 22:47:46 +0200

    init game

--- public/dist/game.js
+++ public/dist/game.js
@@ -3,85 +3,185 @@ class Paddle {
         this.x = x;
         this.y = y;
         this.color = color;
-        this.dir = 'NONE';
+        this.upPressed = false;
+        this.downPressed = false;
     }
     draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x - 5, this.y - 20, 10, 40);
     }
     moveUp() {
-        if (this.y - 10 > 0)
+        if (this.y - 10 > 0 + 10)
             this.y -= 10;
     }
-    moveDown() {
-        if (this.y + 10 < canva.height)
+    moveDown(height) {
+        if (this.y + 10 < height - 10)
             this.y += 10;
+    }
+    reset(y) {
+        this.y = y;
     }
     clear(ctx) {
         ctx.clearRect(this.x - 5, this.y - 20, 10, 40);
     }
 }
-let leftPaddle;
-let rightPaddle;
-let canva;
-const keyDown = (key) => {
-    const code = key.code;
-    if (code === 'KeyS')
-        leftPaddle.dir = 'DOWN';
-    if (code === 'KeyW')
-        leftPaddle.dir = 'UP';
-    if (code === 'ArrowDown')
-        rightPaddle.dir = 'DOWN';
-    if (code === 'ArrowUp')
-        rightPaddle.dir = 'UP';
-};
-const KeyUp = (key) => {
-    const code = key.code;
-    if (code == 'KeyS' || code == 'KeyW')
-        leftPaddle.dir = 'NONE';
-    if (code == 'ArrowDown' || code == 'ArrowUp')
-        rightPaddle.dir = 'NONE';
-};
-const update = (ctx) => {
-    ctx.clearRect(0, 0, canva.width, canva.height);
-    ctx.strokeStyle = 'white';
-    ctx.setLineDash([5]);
-    ctx.moveTo(canva.width / 2, 0);
-    ctx.lineTo(canva.width / 2, canva.height);
-    ctx.stroke();
-    if (leftPaddle.dir != 'NONE') {
-        if (leftPaddle.dir == 'UP')
-            leftPaddle.moveUp();
-        else
-            leftPaddle.moveDown();
+class Ball {
+    constructor(x, y, dx, dy) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
     }
-    if (rightPaddle.dir != 'NONE') {
-        if (rightPaddle.dir == 'UP')
-            rightPaddle.moveUp();
-        else
-            rightPaddle.moveDown();
+    draw(ctx) {
+        ctx.setLineDash([]);
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
     }
-    leftPaddle.draw(ctx);
-    rightPaddle.draw(ctx);
-    ctx.setLineDash([]);
-    ctx.beginPath();
-    ctx.arc(canva.width / 2, canva.height / 2, 10, 0, Math.PI * 2);
-    ctx.fill();
-    requestAnimationFrame(() => update(ctx));
-};
+    update(canvas) {
+        this.x += this.dx;
+        this.y += this.dy;
+        if (this.y + this.dy >= canvas.height || this.y + this.dy <= 0)
+            this.dy = -this.dy;
+    }
+    checkCollision(paddle) {
+        return (this.x + this.dx < paddle.x + 10 &&
+            this.x + this.dx > paddle.x - 10 &&
+            this.y > paddle.y - 30 &&
+            this.y < paddle.y + 30);
+    }
+    reset(canvas) {
+        this.x = canvas.width / 2;
+        this.y = canvas.height / 2;
+    }
+}
+class Game {
+    constructor(canvas) {
+        this.handleKeyDown = (e) => {
+            if (!this.gameStart)
+                this.gameStart = true;
+            switch (e.code) {
+                case 'KeyW':
+                    this.leftPaddle.upPressed = true;
+                    break;
+                case 'KeyS':
+                    this.leftPaddle.downPressed = true;
+                    break;
+                case 'ArrowUp':
+                    this.rightPaddle.upPressed = true;
+                    break;
+                case 'ArrowDown':
+                    this.rightPaddle.downPressed = true;
+                    break;
+            }
+        };
+        this.handleKeyUp = (e) => {
+            switch (e.code) {
+                case 'KeyW':
+                    this.leftPaddle.upPressed = false;
+                    break;
+                case 'KeyS':
+                    this.leftPaddle.downPressed = false;
+                    break;
+                case 'ArrowUp':
+                    this.rightPaddle.upPressed = false;
+                    break;
+                case 'ArrowDown':
+                    this.rightPaddle.downPressed = false;
+                    break;
+            }
+        };
+        this.loop = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.drawMap();
+            if (this.gameStart) {
+                this.updatePaddles();
+                this.updateBall();
+            }
+            this.leftPaddle.draw(this.ctx);
+            this.rightPaddle.draw(this.ctx);
+            this.ball.draw(this.ctx);
+            requestAnimationFrame(this.loop);
+        };
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.leftPaddle = new Paddle(((canvas.width / 2) / 2) - 80, canvas.height / 2, 'white');
+        this.rightPaddle = new Paddle((canvas.width / 2) + ((canvas.width / 2) / 2) + 80, canvas.height / 2, 'white');
+        this.ball = new Ball(canvas.width / 2, canvas.height / 2, 5, 5);
+        this.leftScore = 0;
+        this.rightScore = 0;
+        this.gameStart = false;
+    }
+    drawMap() {
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = 'white';
+        this.ctx.setLineDash([5]);
+        this.ctx.moveTo(this.canvas.width / 2, 0);
+        this.ctx.lineTo(this.canvas.width / 2, this.canvas.height);
+        this.ctx.stroke();
+        this.ctx.closePath();
+        this.ctx.setLineDash([]);
+        this.ctx.strokeRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+    updatePaddles() {
+        if (this.leftPaddle.upPressed)
+            this.leftPaddle.moveUp();
+        if (this.leftPaddle.downPressed)
+            this.leftPaddle.moveDown(this.canvas.height);
+        if (this.rightPaddle.upPressed)
+            this.rightPaddle.moveUp();
+        if (this.rightPaddle.downPressed)
+            this.rightPaddle.moveDown(this.canvas.height);
+    }
+    updateScoreDisplay() {
+        const leftScoreElem = document.getElementById('score-left');
+        const rightScoreElem = document.getElementById('score-right');
+        if (leftScoreElem)
+            leftScoreElem.textContent = String(this.leftScore);
+        if (rightScoreElem)
+            rightScoreElem.textContent = String(this.rightScore);
+    }
+    updateBall() {
+        const prevX = this.ball.x;
+        this.ball.update(this.canvas);
+        if (this.ball.checkCollision(this.leftPaddle) || this.ball.checkCollision(this.rightPaddle)) {
+            this.ball.dx = -this.ball.dx;
+        }
+        if (prevX > 0 && this.ball.x <= 0) {
+            this.rightScore++;
+            this.updateScoreDisplay();
+            this.ball.reset(this.canvas);
+            this.rightPaddle.reset(this.canvas.height);
+            this.leftPaddle.reset(this.canvas.height);
+            this.gameStart = false;
+        }
+        else if (prevX < this.canvas.width && this.ball.x >= this.canvas.width) {
+            this.leftScore++;
+            this.updateScoreDisplay();
+            this.ball.reset(this.canvas);
+            this.rightPaddle.reset(this.canvas.height / 2);
+            this.leftPaddle.reset(this.canvas.height / 2);
+            this.gameStart = false;
+        }
+    }
+    start() {
+        document.addEventListener('keydown', this.handleKeyDown);
+        document.addEventListener('keyup', this.handleKeyUp);
+        this.loop();
+    }
+    stop() {
+        document.removeEventListener('keydown', this.handleKeyDown);
+        document.removeEventListener('keyup', this.handleKeyUp);
+    }
+}
 export const gameInit = () => {
-    canva = document.getElementById('game');
-    const container = canva.parentElement;
-    if (canva && container) {
-        canva.width = container.clientWidth - 10;
-        canva.height = canva.width * (9 / 16);
+    const canvas = document.getElementById('game');
+    const container = canvas.parentElement;
+    if (canvas && container) {
+        canvas.width = container.clientWidth - 10;
+        canvas.height = canvas.width * (9 / 16);
     }
-    const ctx = canva?.getContext('2d');
-    leftPaddle = new Paddle(((canva.width / 2) / 2) - 80, canva.height / 2, 'white');
-    rightPaddle = new Paddle((canva.width / 2) + ((canva.width / 2) / 2) + 80, canva.height / 2, 'white');
-    if (ctx) {
-        document.addEventListener('keydown', keyDown);
-        document.addEventListener('keyup', KeyUp);
-        requestAnimationFrame(() => update(ctx));
-    }
+    const game = new Game(canvas);
+    game.start();
 };
