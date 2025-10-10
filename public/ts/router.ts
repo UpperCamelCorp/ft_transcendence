191dee61b6f41b89140d2aba83abb926be97702a
Arasaka <otxoa.lardeux.pro@gmail.com>
2025-10-10 17:55:10 +0200

    add setNamePage to the router & 404

--- public/ts/router.ts
+++ public/ts/router.ts
@@ -1,5 +1,6 @@
 import { setupSidebar } from "./sidebar.js";
 import { setupHeader } from "./header.js";
+import { notFound } from "./404page.js";
 
 export default class Router {
     private routes: Map<string, () => void>;
@@ -11,14 +12,34 @@ export default class Router {
         this.cleanUp = new Map();
     }
 
+    private setNamePage(name : string) : void {
+        if (window.innerWidth >= 768) {
+            const nameSpan = document.getElementById('page-name') as HTMLSpanElement;
+            if (name) 
+                nameSpan.textContent = name;
+            else
+                nameSpan.textContent = '';
+        }
+        else {
+            const titlePage = document.getElementById('title-page') as HTMLAnchorElement;
+            if (name)
+                titlePage.textContent = name;
+            else
+                titlePage.textContent = 'Transcendence';
+        }
+    }
+
     private handleRoute() : void {
         const path = window.location.pathname;
         const handler = this.routes.get(path);
         setupSidebar();
         setupHeader();
         if (handler){
+            this.setNamePage(path.slice(1).toUpperCase());
             handler();
         } else {
+            this.setNamePage('');
+            notFound();
             console.log(`No route for ${path}`);
         }
     }
