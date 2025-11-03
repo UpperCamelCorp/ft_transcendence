99c1591cd4911cf2a2de1cdefb5e125324a795c6
andymalgonne <andymalgonne@gmail.com>
2025-11-03 12:02:26 +0100

    add rules for local_games metrics

--- api/game/game.js
+++ api/game/game.js
@@ -1,16 +1,51 @@
 const GameManager = require('./GameManager');
+const client = require('prom-client');
+
+const localActiveGauge = new client.Gauge({
+    name: 'pong_local_active_games',
+    help: 'Number of active local Pong games'
+});
+const localGamesStartedCounter = new client.Counter({
+    name: 'pong_local_games_started_total',
+    help: 'Total number of local Pong games started'
+});
 
 const usernameCheck = (username) => {
     const regex = new RegExp(/^[a-zA-Z0-9]+$/);
     return (regex.test(username) && username.length <= 10)
 }
 
-// ...existing code...
 // @ts-check
 /** @param {import('fastify').FastifyInstance} fastify */
 const game = async (fastify, options) => {
-    
+
     const gameManager = new GameManager(fastify.db);
+
+    fastify.post('/api/metrics/local', async (req, rep) => {
+        try {
+            const { event } = req.body || {};
+            if (event === 'start') {
+                localGamesStartedCounter.inc();
+                localActiveGauge.inc();
+                return rep.code(200).send({ ok: true });
+            } else if (event === 'stop') {
+                try {
+                    const metrics = await client.register.getMetricsAsJSON();
+                    const metric = metrics.find(m => m.name === 'pong_local_active_games');
+                    const current = metric && metric.values && metric.values[0] ? metric.values[0].value : 0;
+                    localActiveGauge.set(Math.max(0, current - 1));
+                } catch (e) {
+                    localActiveGauge.dec();
+                }
+                return rep.code(200).send({ ok: true });
+            } else {
+                return rep.code(400).send({ message: 'invalid event' });
+            }
+        } catch (e) {
+            fastify.log.error('metrics error', e);
+            return rep.code(500).send({ message: 'metrics error' });
+        }
+    });
 
     fastify.get('/game/play', {websocket : true}, (connection, req) => {
         let playerIndex;
@@ -58,13 +93,13 @@ const game = async (fastify, options) => {
                         if (data.action === 'Down') {
                             if (data.dir === 'Up')
                                 game.inputs[playerIndex].up = true;
-                            else 
+                            else
                                 game.inputs[playerIndex].down = true;
                         }
                         else {
                             if (data.dir === 'Up')
                                 game.inputs[playerIndex].up = false;
-                            else 
+                            else
                                 game.inputs[playerIndex].down = false;
                         }
                     }
